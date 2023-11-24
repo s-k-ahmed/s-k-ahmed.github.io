8f45bd8609462bc47ef3a05c0022218668c49eef
Sam <samuelkahmed@gmail.com>
2023-11-24 13:05:16 +0100

    WH migration to its own repository

--- woordhex/lists/lijstkorting.py
+++
@@ -1,6 +0,0 @@
-bwFile = open("C:/filenamehere.txt", "r")
-bwText = bwFile.read()
-bwList = bwText.split("',\n\t'")
-from wordfreq import top_n_list
-nl10kList = top_n_list('nl', 10000)
-print(set(bwList) & set(nl10kList))
